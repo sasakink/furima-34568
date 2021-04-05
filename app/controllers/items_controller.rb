b285c970591dbaceba5ed921a1872d280a40a913
sasakink <78790061+sasakink@users.noreply.github.com>
2021-04-05 17:23:31 +0900

    商品出品ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,29 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
-
   def index
+    @items = Item.all
+    # @items = Item.order("created_at DESC")
   end
+
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :item_condition_id, :delivery_fee_id, :delivery_source_id,
+                                 :delivery_date_id, :selling_price, :user, :image).merge(user_id: current_user.id)
   end
 end
