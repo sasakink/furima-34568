73a250807c6de2ef170151a4a5cae021be414b47
sasakink <78790061+sasakink@users.noreply.github.com>
2021-04-05 17:24:51 +0900

    Itemモデルのテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name               { 'test' }
+    description        { 'test' }
+    category_id        { '1' }
+    item_condition_id  { '1' }
+    delivery_fee_id    { '1' }
+    delivery_source_id { '1' }
+    delivery_date_id   { '1' }
+    selling_price      { '1000' }
   end
 end
