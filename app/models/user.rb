f0624041dbc5bd4b0ab6e13cde05b2529fac7a3b
sasakink <78790061+sasakink@users.noreply.github.com>
2021-04-06 15:37:25 +0900

    Userモデルのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
-    validates :first_name_kana
     validates :birthday
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
