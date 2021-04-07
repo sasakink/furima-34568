e69f4fbf20ce5dfa2a2e2b535d3cab6e3a32cb11
sasakink <78790061+sasakink@users.noreply.github.com>
2021-04-07 18:55:41 +0900

    itemコントローラ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
+  def new
+    @item = Item.new
+  end
+
   def index
     @items = Item.all
     # @items = Item.order("created_at DESC")
-  end
-
-  def new
-    @item = Item.new
   end
 
   def create
